2957e2ef7b0a8ef0c52693ce3a49eb713537ab0f
Alan Rios <riosalan264@gmail.com>
2024-05-24 19:01:57 -0300

    Modifique el register para un fix del user

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -28,11 +27,6 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Ruta para registrar un nuevo usuario
-const generateToken = (userId) => {
-  // Generar el token JWT con el ID de usuario y la clave secreta
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' }); // expiresIn es opcional, define el tiempo de expiración del token
-};
-
 app.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -40,7 +34,12 @@ app.post('/register', async (req, res) => {
     // Verificar si el usuario ya existe
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      // Actualizar los detalles del usuario existente con los nuevos datos
+      existingUser.email = email;
+      existingUser.password = hashedPassword;
+      await existingUser.save();
+      
+      return res.status(200).json({ message: 'User details updated successfully', user: existingUser });
     }
  
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -49,19 +48,13 @@ app.post('/register', async (req, res) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
-    // Generar el token JWT
-    const token = generateToken(newUser._id); // Suponiendo que el ID del usuario es _id
-
-    // Eliminar el usuario anterior
-    await User.deleteMany({ createdAt: { $lt: newUser.createdAt } });
-
-    // Enviar la respuesta con el token
-    res.status(201).json({ message: 'User registered successfully', token });
+    res.status(201).json({ message: 'User registered successfully', user: newUser });
   } catch (error) {
     console.error('Error registering user:', error);
     res.status(500).json({ message: 'Error registering user', error });
   }
 });
+
 
 app.get('/getLastUser', async (req, res) => {
   try {
